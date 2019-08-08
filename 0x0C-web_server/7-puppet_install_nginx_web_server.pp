a83154b63e1cc41fbcd6123231561f7aa32436de
Farrukh <679@holbertonschool.com>
2019-08-08 19:07:36 +0000

    Remove some lines

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,21 +3,29 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { '/etc/nginx/sites-available/default':
+file_line { 'Add redirection, 301':
   ensure   => 'present',
+  path     => '/etc/nginx/sites-available/default',
   after    => 'listen 80 default_server;',
   multiple => true,
   line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
+# file_line { '/etc/nginx/sites-available/default':
+#   ensure   => 'present',
+#   after    => 'listen 80 default_server;',
+#   multiple => true,
+#   line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+# }
+
 file { '/var/www/html/index.html':
-  mode    => '0744',
+  #mode    => '0744',
   content => 'Holberton School',
 }
 
 service { 'nginx':
   ensure  => running,
-  enable  => true,
+  #enable  => true,
   require => Package['nginx'],
 }
 
@@ -25,14 +33,6 @@ service { 'nginx':
 #   ensure  => stopped,
 #   enable  => true,
 #   require => Package['nginx'],
-# }
-
-# file_line { 'Add redirection, 301':
-#   ensure   => 'present',
-#   path     => '/etc/nginx/sites-available/default',
-#   after    => 'listen 80 default_server;',
-#   multiple => true,
-#   line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 # }
 
 
