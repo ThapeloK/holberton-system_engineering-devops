ffcc7eff3a2111269571f4d8962f6dc545706821
Farrukh <679@holbertonschool.com>
2019-08-08 19:11:35 +0000

    Add title as path

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,20 +3,12 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'Add redirection, 301':
+file_line { '/etc/nginx/sites-available/default':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
+  #path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
-
-# file_line { 'Add redirection, 301':
-#   ensure   => 'present',
-#   path     => '/etc/nginx/sites-available/default',
-#   after    => 'listen 80 default_server;',
-#   multiple => true,
-#   line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-# }
 
 file { '/var/www/html/index.html':
   content => 'Holberton School',
