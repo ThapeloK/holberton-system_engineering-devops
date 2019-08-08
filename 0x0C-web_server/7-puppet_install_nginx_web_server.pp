772f5cc14788a6cb040faf3949176bd2219cfbf5
Farrukh <679@holbertonschool.com>
2019-08-08 19:13:25 +0000

    Add title as path

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,9 +3,9 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { '/etc/nginx/sites-available/default':
+file_line { 'Add redirection, 301':
   ensure => 'present',
-  #path   => '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
