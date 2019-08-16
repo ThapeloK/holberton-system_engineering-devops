a29820492165d6f25d6caa6c86b4e523d140b872
Farrukh <679@holbertonschool.com>
2019-08-16 22:06:15 +0000

    Add update

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,29 +2,29 @@
 
 $str = "add_header X-Served-By ${hostname};"
 
-package { 'nginx':
-  ensure => installed,
+exec { 'update':
+  command => '/usr/bin/apt-get update',
 }
-
-file_line { 'Add redirection, 301':
+-> package { 'nginx':
+  ensure  => installed,
+  require => Exec['update']
+}
+-> file_line { 'Add redirection, 301':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
-
-file_line { 'Add custom HTTP server':
+-> file_line { 'Add custom HTTP server':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => $str,
 }
-
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
-
-service { 'nginx':
+-> service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
