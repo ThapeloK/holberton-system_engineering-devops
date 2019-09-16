b2bfc1a522fe14f378e0d99bd334870af1466996
Farrukh <679@holbertonschool.com>
2019-09-16 23:01:49 +0000

    add main

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,14 +4,16 @@ import json
 import requests
 
 
-r = requests.get('https://jsonplaceholder.typicode.com/users')
-users = r.json()
-r = requests.get('https://jsonplaceholder.typicode.com/todos')
-tasks = r.json()
-dic = {str(d.get('id')): [{'username': d.get('username'),
-                           'task': i.get('title'), 'completed':
-                           i.get('completed')}
-                          for i in tasks
-                          if i.get('userId') == d.get('id')] for d in users}
-with open('todo_all_employees.json', 'w') as json_file:
-    json.dump(dic, json_file)
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = r.json()
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = r.json()
+    dic = {str(d.get('id')): [{'username': d.get('username'),
+                               'task': i.get('title'), 'completed':
+                               i.get('completed')}
+                              for i in tasks
+                              if i.get('userId') == d.get('id')]
+           for d in users}
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(dic, json_file)
