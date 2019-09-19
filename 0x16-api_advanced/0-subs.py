e205b5f04025a673df3b65b119c8586de71914f6
Farrukh <679@holbertonschool.com>
2019-09-19 19:09:34 +0000

    Remove print

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,6 @@ def number_of_subscribers(subreddit):
     """Get request"""
     base_url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
     h = {'User-Agent': 'Reddit API test'}
-    print(base_url)
     r = requests.get(base_url, headers=h, allow_redirects=False)
     d = r.json()
     if d.get('error', 200) == 404:
