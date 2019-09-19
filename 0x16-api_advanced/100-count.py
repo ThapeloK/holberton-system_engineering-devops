a635dbce41e0b970a5faf368db81df410d4e19a2
Farrukh <679@holbertonschool.com>
2019-09-19 21:14:54 +0000

    Add task 100

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Get request to reddit API"""
+import requests
+
+
+def recurse(subreddit, word_list, after=1, dic={}):
+    """Recursive function"""
+    base_url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    h = {'User-Agent': 'Reddit API test'}
+    params = {'limit': 200, 'after': after}
+    r = requests.get(base_url, headers=h, allow_redirects=False, params=params)
+    if r.status_code > 299:
+        return None
+    d = r.json()
+    if after is None:
+        return dic
+    l = d.get('data').get('children')
+    for i in l:
+        title = i.get('data').get('title').lower()
+        for j in word_list:
+            if j in title:
+                if j not in dic:
+                    dic[j] = 1
+                else:
+                    dic[j] += 1
+    p = d.get('data').get('after')
+    return recurse(subreddit, word_list, p, dic)
+
+
+def count_words(subreddit, word_list):
+    """Cound # of keywords"""
+    for i, e in enumerate(word_list):
+        word_list[i] = e.lower()
+    d = recurse(subreddit, word_list)
+    if d:
+        for key, value in sorted(d.items(), key=lambda x: x[1], reverse=True):
+            print("{}: {}".format(key, value))
