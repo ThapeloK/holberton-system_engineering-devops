a9d261eae1b0df82b5d9876e083e3d6e04ad36f4
Farrukh <679@holbertonschool.com>
2019-09-19 19:24:14 +0000

    Show 10 top news

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Get request to reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """Get request"""
+    base_url = 'https://www.reddit.com/r/{}/top/.json'.format(subreddit)
+    h = {'User-Agent': 'Reddit API test'}
+    params = {'limit': 10}
+    r = requests.get(base_url, headers=h, allow_redirects=False, params=params)
+    d = r.json()
+    if d.get('error', 200) == 404:
+        print(None)
+        return
+    l = d.get('data').get('children')
+    for dic in l:
+        print(dic.get('data').get('title'))
